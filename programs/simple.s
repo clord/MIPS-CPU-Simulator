fbedbf3cc2d48295bfebd1bf1076c5ead3fee1ed
Christopher Lord <christopherlord@gmail.com>
2011-04-28 22:45:14 -0400

    spacing changes for sample input

--- programs/simple.s
+++ programs/simple.s
@@ -1,19 +1,22 @@
 .text
 
-main:	li   $4, 32
+main:
+	li   $4, 32
 	subi $5, $4, 1
 	addi $3, $4, 2
 	la   $6, seg
 	b exit
 	subi $5, $4, 1
 	addi $3, $4, 2
-	lb   $7, 1($6)			
+	lb   $7, 1($6)
 	nop
 	nop
-			
-exit:	li $2, 10
+
+exit:
+	li $2, 10
 	syscall
-	
+
 .data
 
-seg: 	.asciiz "atest\n"
+seg:
+	.asciiz "atest\n"
--- programs/vector.s
+++ programs/vector.s
@@ -1,13 +1,15 @@
 
 .text
 
-main:	li $1, 0
+main:
+	li $1, 0
 	li $2, 10
 	li $3, 100
 	li $4, 1000
 	li $5, 7
 
-loop:	subi $5, $5, 1
+loop:
+	subi $5, $5, 1
 	add $1, $1, $2
 	add $1, $1, $3
 	add $1, $1, $4
